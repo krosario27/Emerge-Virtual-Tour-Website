498f0c107455d19e4fc202d1611ecbbca660eae4
Kyle White <kylewhite369@gmail.com>
2024-06-12 11:36:57 +1200

    Modified a folder

--- Emerge/Emerge/settings.py
+++ Emerge/Emerge/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,5 +126,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-import django_on_heroku
-django_on_heroku.settings(locals())
